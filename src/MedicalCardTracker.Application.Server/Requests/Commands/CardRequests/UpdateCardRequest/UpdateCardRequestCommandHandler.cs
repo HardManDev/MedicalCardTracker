8e37ed849e3a02ebd451d84cba07f1bb5fe13313
HardManDev <hardman.dev@protonmail.com>
2023-04-12 20:22:14 +0300

    Created MediatR request handlers for `CardRequest` entity

---
+++ src/MedicalCardTracker.Application.Server/Requests/Commands/CardRequests/UpdateCardRequest/UpdateCardRequestCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using AutoMapper;
+using MediatR;
+using MedicalCardTracker.Application.Models.ViewModels;
+using MedicalCardTracker.Application.Requests.Commands.CardRequests.UpdateCardRequest;
+using MedicalCardTracker.Application.Server.Exceptions;
+using MedicalCardTracker.Application.Server.Interfaces;
+using MedicalCardTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalCardTracker.Application.Server.Requests.Commands.CardRequests.UpdateCardRequest;
+
+public class UpdateCardRequestCommandHandler
+    : BaseRequestHandler, IRequestHandler<UpdateCardRequestCommand, CardRequestVm>
+{
+    public UpdateCardRequestCommandHandler(IApplicationDbContext dbContext, IMapper mapper)
+        : base(dbContext, mapper)
+    {
+    }
+
+    public async Task<CardRequestVm> Handle(UpdateCardRequestCommand request,
+        CancellationToken cancellationToken)
+    {
+        var targetCardRequest =
+            await DbContext.CardRequests
+                .FirstOrDefaultAsync(item => item.Id == request.Id, cancellationToken)
+            ?? throw new EntityNotFoundException(nameof(CardRequest), request.Id);
+
+        Mapper.Map(request, targetCardRequest);
+        targetCardRequest.UpdatedAt = DateTime.Now;
+
+        await DbContext.SaveChangesAsync(cancellationToken);
+
+        return Mapper.Map<CardRequestVm>(targetCardRequest);
+    }
+}
---
+++ src/MedicalCardTracker.Application.Server/Requests/Queries/CardRequests/GetCardRequestCollection/GetCardRequestCollectionQueryHandler.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using System.Collections.ObjectModel;
+using AutoMapper;
+using MediatR;
+using MedicalCardTracker.Application.Models;
+using MedicalCardTracker.Application.Models.Enums;
+using MedicalCardTracker.Application.Models.ViewModels;
+using MedicalCardTracker.Application.Requests.Queries.CardRequests.GetCardRequestCollection;
+using MedicalCardTracker.Application.Server.Interfaces;
+using MedicalCardTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalCardTracker.Application.Server.Requests.Queries.CardRequests.GetCardRequestCollection;
+
+public class GetCardRequestCollectionQueryHandler
+    : BaseRequestHandler, IRequestHandler<GetCardRequestCollectionQuery, CardRequestCollectionVm>
+{
+    public GetCardRequestCollectionQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        : base(dbContext, mapper)
+    {
+    }
+
+    public async Task<CardRequestCollectionVm> Handle(GetCardRequestCollectionQuery request,
+        CancellationToken cancellationToken)
+    {
+        var query = DbContext.CardRequests.AsQueryable();
+
+        if (!string.IsNullOrEmpty(request.SearchIn) && !string.IsNullOrEmpty(request.SearchQuery))
+            query = request.SearchIn switch
+            {
+                nameof(CardRequest.CustomerName) => query.Where(item => item.CustomerName == request.SearchQuery),
+                nameof(CardRequest.TargetAddress) => query.Where(item => item.TargetAddress == request.SearchQuery),
+                nameof(CardRequest.PatientFullName) => query.Where(item => item.PatientFullName == request.SearchQuery),
+                nameof(CardRequest.Status) => query.Where(item => item.Status.ToString() == request.SearchQuery),
+                nameof(CardRequest.Priority) => query.Where(item => item.Priority.ToString() == request.SearchQuery),
+                _ => query
+            };
+
+        query = request.OrderBy == OrderBy.Descending
+            ? query.OrderByDescending(x => x.CreatedAt)
+            : query.OrderBy(x => x.CreatedAt);
+
+        var totalItems = await query.CountAsync(cancellationToken);
+        var items = await query
+            .Skip((int)request.Page * (int)request.Count)
+            .Take((int)request.Count)
+            .ToListAsync(cancellationToken);
+
+        return Mapper.Map<CardRequestCollectionVm>(
+            new VmCollection<CardRequestVm>
+            {
+                TotalCount = (uint)totalItems,
+                Collection = Mapper.Map<ObservableCollection<CardRequestVm>>(items)
+            });
+    }
+}
