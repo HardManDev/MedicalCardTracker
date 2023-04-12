789b3ce8ab00187e61f5da468b8368d63759f102
HardManDev <hardman.dev@protonmail.com>
2023-04-13 01:48:43 +0300

    Created controller for `CardRequest` entity with CRUD operations

---
+++ src/MedicalCardTracker.Server/Controllers/CardRequestController.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using MedicalCardTracker.Application.Models.ViewModels;
+using MedicalCardTracker.Application.Requests.Commands.CardRequests.CreateCardRequest;
+using MedicalCardTracker.Application.Requests.Commands.CardRequests.DeleteCardRequest;
+using MedicalCardTracker.Application.Requests.Commands.CardRequests.UpdateCardRequest;
+using MedicalCardTracker.Application.Requests.Queries.CardRequests.GetCardRequestById;
+using MedicalCardTracker.Application.Requests.Queries.CardRequests.GetCardRequestCollection;
+using MedicalCardTracker.Application.Server.Exceptions;
+using MedicalCardTracker.Server.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalCardTracker.Server.Controllers;
+
+public class CardRequestController : BaseController
+{
+    [HttpGet]
+    public async Task<ActionResult<CardRequestVm>> Get(
+        [FromQuery] GetCardRequestByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await Mediator.Send(request, cancellationToken);
+        }
+        catch (Exception e)
+        {
+            if (e.GetType() == typeof(EntityNotFoundException)) return NotFound();
+        }
+
+        return BadRequest();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<CardRequestCollectionVm>> GetCollection(
+        [FromQuery] GetCardRequestCollectionQuery request,
+        CancellationToken cancellationToken
+    ) => await Mediator.Send(request, cancellationToken);
+
+    [HttpPost]
+    public async Task<ActionResult<CardRequestVm>> Create(
+        [FromBody] CreateCardRequestCommand request,
+        CancellationToken cancellationToken
+    ) => await Mediator.Send(request, cancellationToken);
+
+    [HttpPatch]
+    public async Task<ActionResult<CardRequestVm>> Update(
+        [FromQuery] Guid id,
+        [FromBody] UpdateCardRequestCommandDto requestCommandDto,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await Mediator.Send(
+                new UpdateCardRequestCommand
+                {
+                    Id = id,
+                    CustomerName = requestCommandDto.CustomerName,
+                    TargetAddress = requestCommandDto.TargetAddress,
+                    PatientFullName = requestCommandDto.PatientFullName,
+                    PatientBirthDate = requestCommandDto.PatientBirthDate,
+                    Description = requestCommandDto.Description,
+                    Status = requestCommandDto.Status,
+                    Priority = requestCommandDto.Priority
+                }, CancellationToken.None);
+        }
+        catch (Exception e)
+        {
+            if (e.GetType() == typeof(EntityNotFoundException)) return NotFound();
+        }
+
+        return BadRequest();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(
+        [FromQuery] Guid id,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            await Mediator.Send(
+                new DeleteCardRequestCommand
+                {
+                    Id = id
+                }, CancellationToken.None);
+
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            if (e.GetType() == typeof(EntityNotFoundException)) return NotFound();
+        }
+
+        return BadRequest();
+    }
+}
---
+++ src/MedicalCardTracker.Server/Models/Dto/UpdateCardRequestCommandDto.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using System.Text.Json.Serialization;
+using MedicalCardTracker.Application.Convertors;
+using MedicalCardTracker.Domain.Enums;
+
+namespace MedicalCardTracker.Server.Models.Dto;
+
+public class UpdateCardRequestCommandDto
+{
+    public string? CustomerName { get; set; }
+    public string? TargetAddress { get; set; }
+
+    public string? PatientFullName { get; set; }
+
+    [JsonConverter(typeof(DateOnlyConverter))]
+    public DateOnly? PatientBirthDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public CardRequestStatus? Status { get; set; }
+    public CardRequestPriority? Priority { get; set; }
+}
