216f37e0014e3ba68e1796d6b27c82f47d3974bb
HardManDev <hardman.dev@protonmail.com>
2023-04-12 19:01:25 +0300

    Created dependency injection for AutoMapper in MedicalCardTracker.Application.csproj

---
+++ src/MedicalCardTracker.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using System.Reflection;
+using MedicalCardTracker.Application.Interfaces;
+using MedicalCardTracker.Application.Mappings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MedicalCardTracker.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services) =>
+        services.AddAutoMapper(config =>
+        {
+            config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(new AssemblyMappingProfile(typeof(IMapWith<>).Assembly));
+        });
+}
