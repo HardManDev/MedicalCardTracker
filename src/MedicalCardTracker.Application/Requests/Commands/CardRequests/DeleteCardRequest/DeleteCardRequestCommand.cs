fee72a560751173756e8935efaa27b566f810bbd
HardManDev <hardman.dev@protonmail.com>
2023-04-12 19:29:59 +0300

    Created MediatR requests for `CardRequest` entity

---
+++ src/MedicalCardTracker.Application/Requests/Commands/CardRequests/DeleteCardRequest/DeleteCardRequestCommand.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using MediatR;
+using MedicalCardTracker.Application.Attributes;
+
+namespace MedicalCardTracker.Application.Requests.Commands.CardRequests.DeleteCardRequest;
+
+public class DeleteCardRequestCommand : IRequest
+{
+    [QueryParameter] public Guid Id { get; set; }
+}
---
+++ src/MedicalCardTracker.Application/Requests/Queries/CardRequests/GetCardRequestById/GetCardRequestByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using MediatR;
+using MedicalCardTracker.Application.Attributes;
+using MedicalCardTracker.Application.Models.ViewModels;
+
+namespace MedicalCardTracker.Application.Requests.Queries.CardRequests.GetCardRequestById;
+
+public class GetCardRequestByIdQuery : IRequest<CardRequestVm>
+{
+    [QueryParameter] public Guid Id { get; set; }
+}
