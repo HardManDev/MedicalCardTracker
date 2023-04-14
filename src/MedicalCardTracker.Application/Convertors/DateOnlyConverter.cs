48c4d4513226781097556b67c9408b10320a694d
HardManDev <hardman.dev@protonmail.com>
2023-04-14 12:22:13 +0300

    Added logic for convert to DateOnly from string

--- src/MedicalCardTracker.Application/Convertors/DateOnlyConverter.cs
+++ src/MedicalCardTracker.Application/Convertors/DateOnlyConverter.cs
@@ -11,10 +11,20 @@ public class DateOnlyConverter : JsonConverter<DateOnly?>
 {
     public override DateOnly? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var dateObj = JsonSerializer.Deserialize<DateOnlyObject>(ref reader, options);
-        if (dateObj == null) return null;
-
-        return new DateOnly(dateObj.Year, dateObj.Month, dateObj.Day);
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.Null:
+                return null;
+            case JsonTokenType.String when DateOnly.TryParse(reader.GetString(), out var date):
+                return date;
+            case JsonTokenType.StartObject:
+            {
+                var dateObj = JsonSerializer.Deserialize<DateOnlyObject>(ref reader, options);
+                return new DateOnly(dateObj.Year, dateObj.Month, dateObj.Day);
+            }
+            default:
+                throw new JsonException($"Cannot convert {reader.TokenType} to {typeof(DateOnly)}");
+        }
     }
 
     public override void Write(Utf8JsonWriter writer, DateOnly? value, JsonSerializerOptions options)
