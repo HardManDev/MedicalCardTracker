1f24ce6998ded3764b1f715c08b5fcc5f058185e
HardManDev <hardman.dev@protonmail.com>
2023-04-16 01:02:20 +0300

    Added logger for client

--- src/MedicalCardTracker.Client/App.xaml.cs
+++ src/MedicalCardTracker.Client/App.xaml.cs
@@ -4,11 +4,14 @@
 
 using System;
 using System.IO;
+using System.Reflection;
 using System.Windows;
 using MedicalCardTracker.Application;
 using MedicalCardTracker.Application.Client.Configuration;
 using MedicalCardTracker.Application.Client.Requests;
+using MedicalCardTracker.Application.Logging;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace MedicalCardTracker.Client;
 
@@ -34,6 +37,11 @@ public partial class App : System.Windows.Application
 
     protected override void OnStartup(StartupEventArgs e)
     {
+        var configuration = _serviceProvider.GetRequiredService<ApplicationConfiguration>();
+
+        if (configuration.IsWriteLog)
+            Log.Logger = Assembly.GetExecutingAssembly().GetLogger();
+
         base.OnStartup(e);
     }
 }
